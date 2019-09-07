ec0f1640ddd2ce5bd1b57b1c09ba1b2fb36c7c68
Ahmed Eldakhly <ahmedeldakhlly94@gmail.com>
2019-09-07 15:13:22 +0200

    KeyPad_file

--- KEYPAD.c
+++ KEYPAD.c
@@ -5,49 +5,49 @@
  *  Author: hossam
  */ 
 #include "DIO.h"
+#include "KEYPAD.h"
 
 /*Local Symbols*/
-#define KPD_COL_PORT PD
-#define KPD_ROW_PORT PD
-#define KPD_COL_MASK 0x70
-#define KPD_ROW_MASK 0x0f
-#define KPD_COL_PIN_NUM 4u
-#define KPD_ROW_PIN_NUM 0u
+#define kpdD_COL_PORT (PD)
+#define KPd_ROW_PORT (PD)
+#define KpPD_COL_MASK (0x70)
+#define kPD_ROW_MASK (0x0f)
+#define KPD_COL_PIN_NUM (4u)
+#define KPD_ROW_PIN_NUM (0u)
 /**************************************************/
-#define KPD_COL_INIT() DIO_vidWritePortDirection(KPD_COL_PORT,KPD_COL_MASK,0x00)
-#define KPD_ROW_INIT() DIO_vidWritePortDirection(KPD_ROW_PORT,KPD_ROW_MASK,0xff); \
-                       DIO_vidWritePortData(KPD_ROW_PORT,KPD_ROW_MASK,0x00)
-
-#define KPD_COL_READ(VALPTR) DIO_vidReadPortData(KPD_COL_PORT,KPD_COL_MASK,(VALPTR));\
-                             *(VALPTR) = (*(VALPTR)) >> KPD_COL_PIN_NUM
-
-
-#define KPD_ROW_WRITE(DATA) DIO_vidWritePortData(KPD_ROW_PORT,KPD_ROW_MASK,((DATA) << KPD_ROW_PIN_NUM))
  
-static const unsigned char KeysLut[]= {'1' , '2' , '3' , '4' , '5' , '6','7' , '8' , '9','*' , '0' , '#'};
+
 void KPD_Init(void)
 {
-	KPD_COL_INIT();
-	KPD_ROW_INIT();
+    DIO_vidWritePortDirection((u8)kpdD_COL_PORT,(u8)KpPD_COL_MASK,(u8)0x00);
+    DIO_vidWritePortDirection((u8)KPd_ROW_PORT,(u8)kPD_ROW_MASK,(u8)0xff);
+    DIO_vidWritePortData((u8)KPd_ROW_PORT,(u8)kPD_ROW_MASK,(u8)0x00);
 	
 }
-void KPD_ReadVal(char* ValuePtr)
+void KPD_ReadVal(u8* ValuePtr)
 {
-	unsigned char Rowdata;
-	unsigned char ColData;
-	unsigned char LoopTermnate = 0;
-	for(Rowdata = 0 ; (Rowdata < 4) & (LoopTermnate == 0) ; Rowdata ++)
+    static const u8 KeysLut[]= { '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9' , '*' , '0' , '#' };
+	u8 Rowdata;
+	u8 Col_Data;
+	u8 LoopTermnate = 0u;
+	for(Rowdata = 0u ; ((Rowdata < 4u) && (LoopTermnate == 0u)) ; Rowdata ++)
 	{
-		KPD_ROW_WRITE((1<<Rowdata));
-		KPD_COL_READ(&ColData);
-		if(ColData != 0)
+	    u8 j;
+	    j=((u8)1<<Rowdata);
+	    u8 f=(j<< KPD_ROW_PIN_NUM);
+	    DIO_vidWritePortData((u8)KPd_ROW_PORT,(u8)kPD_ROW_MASK,(u8)f);
+	    DIO_vidReadPortData((u8)kpdD_COL_PORT,(u8)KpPD_COL_MASK,(&Col_Data));
+	    *(&Col_Data) = (*(&Col_Data)) >> KPD_COL_PIN_NUM;
+		if(Col_Data != 0u)
 		{
-			*ValuePtr = KeysLut[(Rowdata*3) + ColData/2];
-			LoopTermnate = 1;
+		    u16 o=Rowdata*3u;
+		    u8 i=Col_Data/2u;
+			*ValuePtr = KeysLut[(u8)(o) + i];
+			LoopTermnate = 1u;
 		}
 		else
 		{
-			*ValuePtr = 'n';
+			*ValuePtr =(u8) 'n';
 		}
 	}
 
