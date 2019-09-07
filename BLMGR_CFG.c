ab75a290162c7e495ed29a9452129c5e3ff4bb5d
Ahmed Eldakhly <ahmedeldakhlly94@gmail.com>
2019-09-07 14:56:53 +0200

    BLMGR_CFG_file

--- BLMGR_CFG.c
+++ BLMGR_CFG.c
@@ -6,7 +6,8 @@
  */ 
 #include "DIO.h"
 #include "BLMGR_CFG.h"
-BLMGR_DioPinConfig BuzzerConfig      = {PC,1<<4};
-BLMGR_DioPinConfig BlueToothPwrConfig = {PC,1<<5};
-BLMGR_DioPinConfig BluetoothKeyConfig = {PD,1<<2};	
+/*casting can be by unsigned or u */
+BLMGR_DioPinConfig BuzzerConfig      = {PC,(1u)<<4};
+BLMGR_DioPinConfig BlueToothPwrConfig = {PC,((unsigned)1u)<<5};
+BLMGR_DioPinConfig BluetoothKeyConfig = {PD,(unsigned)1<<2};
 	
